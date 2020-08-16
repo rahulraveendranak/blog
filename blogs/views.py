942d05cf0096aa6040d51d6f82338a6e5bd1612c
Rahul <rahular60@gmail.com>
2020-08-16 22:41:39 +0530

    model created

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,HttpResponse
-
+from blogs.models import blog
 # Create your views here.
 def home(request):
     return render(request,'index.html')
 
 def blog(request):
-    return render(request,'bloghome.html')
+    blogs = blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request,'bloghome.html', context)
 
 def blogpost(request,slug):
     return HttpResponse(f"you are viewing{slug}")
