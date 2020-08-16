3c20f0488a6596b80ca5d08362a83ed5d55ee597
Rahul <rahular60@gmail.com>
2020-08-16 13:42:40 +0530

    added templates

--- blogs/urls.py
+++ blogs/urls.py
@@ -19,6 +19,8 @@ from blogs import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog', views.blog, name='blog'),
-    path('contact', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+    path('blogpost/<str:slug>', views.blogpost, name='blogpost'),
+    path('contact/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
+    return render(request,'index.html')
 
 def blog(request):
-    return HttpResponse("blog")
+    return render(request,'bloghome.html')
+
+def blogpost(request,slug):
+    return HttpResponse(f"you are viewing{slug}")
 
 def contact(request):
-    return HttpResponse("contact")
+    return render(request,'contact.html')
+
+def search(request):
+    return render(request,'search.html')
